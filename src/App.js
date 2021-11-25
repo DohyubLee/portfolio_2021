f3f96624ad8ec8e0b6a13a396e8759e3f9a1ca9f
ldh <hani7304@gmail.com>
2021-11-25 17:38:04 +0900

    react router, axios 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.scss";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">ddd</header>
+      <Routes>
+        <Route index element={<h3>Please select a topic.</h3>} />
+      </Routes>
     </div>
   );
 }
