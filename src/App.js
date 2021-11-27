3c78a523b48965fd8115c72c55a2018fb764012b
ldh <hani7304@gmail.com>
2021-11-27 22:53:42 +0900

    이제 개발 진짜 시작

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import "./App.scss";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Link to="/">index</Link>
-        <Link to="/popular">popular</Link>
-      </header>
+      <Header />
       <Routes>
-        <Route path="/" element={<h3>home9</h3>} />
-        <Route path="popular" element={<h3>popular9</h3>} />
+        <Route path="/" element={<Home />} />
+        <Route path="popular" element={<h3>popular11</h3>} />
       </Routes>
     </div>
   );
