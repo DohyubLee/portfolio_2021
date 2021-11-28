883d23f730e5cc105764386399826bc2a2218089
ldh <hani7304@gmail.com>
2021-11-28 19:34:19 +0900

    외부 박스 반응형 작업, scroll events 처리

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,58 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Header.scss";
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './Header.scss';
 
 const Header = () => {
+  const [navActive, setNavActive] = useState(false);
+
+  const changeNavBackColor = () => {
+    if (window.scrollY > 0) {
+      setNavActive(true);
+    } else {
+      setNavActive(false);
+    }
+  };
+
+  window.addEventListener('scroll', changeNavBackColor);
   return (
-    <header>
-      <div className="App-header">
-        <Link to="/">index</Link>
-        <Link to="/popular">popular</Link>
-      </div>
+    <header className={navActive ? 'header-back-color' : ''}>
+      <nav className="App-header">
+        <div className="left-wrap">
+          <Link to="/">
+            <i class="fas fa-video fa-3x"></i>
+          </Link>
+          <ul>
+            <li>
+              <NavLink
+                to="/now-playing"
+                className={({ isActive }) => (isActive ? 'active' : 'nav-tab')}
+              >
+                now-playing
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/popular"
+                className={({ isActive }) => (isActive ? 'active' : 'nav-tab')}
+              >
+                popular
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/upcoming"
+                className={({ isActive }) => (isActive ? 'active' : 'nav-tab')}
+              >
+                upcoming
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="right-wrap">
+          <button>검색</button>
+          <Link to="/my-list">my-list</Link>
+        </div>
+      </nav>
     </header>
   );
 };
