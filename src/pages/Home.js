ac0231a964086574935f3d2a0ad9cea75d1005fc
ldh <hani7304@gmail.com>
2021-12-08 11:53:08 +0900

    메인 포스터 반응형 작업

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,64 @@
-import React from "react";
+import { Skeleton } from '@mui/material';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import './Home.scss';
 
-const Home = () => {
+const Home = props => {
+  const { imageConfig, api_key } = props;
+  const isMobile = useMediaQuery({ query: '(max-width: 575px)' });
+  const [popularDatas, setPopularDatas] = useState({
+    results: [],
+  });
+
+  useEffect(() => {
+    axios({
+      method: 'get',
+      baseURL: 'https://api.themoviedb.org',
+      url: '/3/movie/popular',
+      params: {
+        api_key,
+        language: 'ko',
+      },
+    }).then(res => {
+      setPopularDatas({ ...res.data });
+    });
+  }, []);
+
   return (
-    <div>
-      <h3> Home3</h3>
+    <div className="home-wrap">
+      <div className="back-image-wrap">
+        {isMobile ? (
+          popularDatas.results.length > 0 ? (
+            <img
+              src={`${imageConfig.base_url}${imageConfig.backdrop_sizes[3]}${popularDatas.results[0].poster_path}`}
+            />
+          ) : (
+            <div className="mob-skelton">
+              <Skeleton sx={{ bgcolor: 'grey.900' }} variant="rectangular" height={'80vh'} />
+            </div>
+          )
+        ) : popularDatas.results.length > 0 ? (
+          <img
+            src={`${imageConfig.base_url}${imageConfig.backdrop_sizes[3]}${popularDatas.results[0].backdrop_path}`}
+          />
+        ) : (
+          <div className="mob-skelton">
+            <Skeleton sx={{ bgcolor: 'grey.900' }} variant="rectangular" height={'80vh'} />
+          </div>
+        )}
+        {popularDatas.results.length > 0 && (
+          <div className="movie-info-box">
+            <div className="movie-info">
+              <div className="title">{popularDatas.results[0].title}</div>
+              <div className="overview">{popularDatas.results[0].overview}</div>
+            </div>
+            <div className="btn-box">
+              <button className="detail-btn">상세보기</button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
