ba6ecdf21d2b1dc282f17a384bdc1f0113907144
ldh <hani7304@gmail.com>
2021-12-08 20:30:44 +0900

    메인 페이지 모바일버전 1차완료

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './Slider.scss';
+import { useMediaQuery } from 'react-responsive';
+import { BrowserView, MobileView } from 'react-device-detect';
+import { Skeleton } from '@mui/material';
 
 const Slider = props => {
-  return <div className="slider-wrap">Slider</div>;
+  const { movieDatas, imageConfig, category } = props;
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1199px)' });
+  const skelDefaultArr = [0, 1, 2, 3, 4, 5, 6, 7];
+
+  //movieDatas.results.length > 0
+  return (
+    <div className="slider-wrap">
+      <MobileView className="mob-view">
+        <div className="category-title">{category}</div>
+        <ul>
+          {movieDatas.results.length > 0 ? (
+            <Fragment>
+              {movieDatas.results.map(data => {
+                return (
+                  <li>
+                    <img
+                      src={`${imageConfig.base_url}${imageConfig.poster_sizes[1]}${data.poster_path}`}
+                    />
+                  </li>
+                );
+              })}
+            </Fragment>
+          ) : (
+            <Fragment>
+              {skelDefaultArr.map((data, index) => {
+                return (
+                  <li>
+                    <Skeleton
+                      className="custom-sk"
+                      sx={{ bgcolor: 'grey.900' }}
+                      variant="rectangular"
+                    />
+                  </li>
+                );
+              })}
+            </Fragment>
+          )}
+        </ul>
+      </MobileView>
+      <BrowserView className="pc-view">WEB</BrowserView>
+    </div>
+  );
 };
 
 export default Slider;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,12 @@ const Home = props => {
   const [popularDatas, setPopularDatas] = useState({
     results: [],
   });
+  const [nowPlayingDatas, setNowPlayingDatas] = useState({
+    results: [],
+  });
+  const [upcomingDatas, setUpcomingDatas] = useState({
+    results: [],
+  });
 
   useEffect(() => {
     axios({
@@ -23,6 +29,28 @@ const Home = props => {
       },
     }).then(res => {
       setPopularDatas({ ...res.data });
+    });
+    axios({
+      method: 'get',
+      baseURL: 'https://api.themoviedb.org',
+      url: '/3/movie/now_playing',
+      params: {
+        api_key,
+        language: 'ko',
+      },
+    }).then(res => {
+      setNowPlayingDatas({ ...res.data });
+    });
+    axios({
+      method: 'get',
+      baseURL: 'https://api.themoviedb.org',
+      url: '/3/movie/upcoming',
+      params: {
+        api_key,
+        language: 'ko',
+      },
+    }).then(res => {
+      setUpcomingDatas({ ...res.data });
     });
   }, []);
 
@@ -60,7 +88,9 @@ const Home = props => {
           </div>
         )}
       </div>
-      <Slider />
+      <Slider imageConfig={imageConfig} movieDatas={popularDatas} category={'인기'} />
+      <Slider imageConfig={imageConfig} movieDatas={nowPlayingDatas} category={'현재 상영 중'} />
+      <Slider imageConfig={imageConfig} movieDatas={upcomingDatas} category={'개봉 예정'} />
     </div>
   );
 };
