61a6c86aff673243649ec3062adff2a67005e4f6
ldh <hani7304@gmail.com>
2021-12-16 20:01:43 +0900

    모바일 헤더 디자인 UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 import './Header.scss';
 import { BrowserView, MobileView, isMobile } from 'react-device-detect';
-import { Link } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
   const [isShow, setIsShow] = useState(true);
+  const [isDrop, setIsDrop] = useState(false);
   let prevScrollpos = window.pageYOffset;
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 767px)' });
+  const mobNavRef = useRef();
+  const navIcRef = useRef();
+
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
-
+    window.addEventListener('mousedown', closeMobNav);
     return () => {
       console.log('컴포넌트 종료');
       window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('mousedown', closeMobNav);
     };
   }, []);
 
@@ -19,36 +26,128 @@ const Header = () => {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
       setIsShow(true);
+      setIsDrop(false);
     } else {
       setIsShow(false);
     }
     prevScrollpos = currentScrollPos;
   };
 
+  const closeMobNav = event => {
+    // 해당 박스 존재유무 파악
+    if (!!mobNavRef.current) {
+      // 해당 박스 영역 외에서 클릭했을시
+      if (!mobNavRef.current.contains(event.target)) {
+        // 아이콘 영역 외에서만 동작시키기위해
+        if (!navIcRef.current.contains(event.target)) {
+          setIsDrop(false);
+        }
+      }
+    }
+  };
+
+  const openDropNav = () => {
+    setIsDrop(!isDrop);
+  };
+
   return (
-    <header className={isShow ? '' : 'header-hide'}>
-      <MobileView className="mob-wrap">
-        <div className="menu">
-          <button>
-            <i className="fas fa-bars"></i>
-          </button>
-        </div>
-        <div className="home">
-          <Link to="/">
-            <i className="fas fa-video"></i>
-          </Link>
-        </div>
-        <div className="search">
-          <button>
-            <i className="fas fa-search"></i>
-          </button>
-          <Link to="/my-list" className="my-list">
-            <i className="fas fa-heart fa-lg"></i>
-          </Link>
-        </div>
-      </MobileView>
-      <BrowserView className="pc-wrap">WEB</BrowserView>
-    </header>
+    <Fragment>
+      {isMobile ? (
+        <header className={isShow ? 'mob-header' : 'mob-header hide'}>
+          <div className="header-items">
+            <div className="left-wrap">
+              {isTabletOrMobile && (
+                <button className="nav-open-btn" onClick={openDropNav} ref={navIcRef}>
+                  <i className="fas fa-bars"></i>
+                </button>
+              )}
+              <Link to="/" className="main-ic">
+                <i className="fas fa-video"></i>
+              </Link>
+              {!isTabletOrMobile && (
+                <ul>
+                  <li>
+                    <NavLink
+                      to="/now-playing"
+                      className={({ isActive }) => (isActive ? 'active' : 'nav-tab')}
+                    >
+                      현재 상영 중
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/popular"
+                      className={({ isActive }) => (isActive ? 'active' : 'nav-tab')}
+                    >
+                      인기
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/upcoming"
+                      className={({ isActive }) => (isActive ? 'active' : 'nav-tab')}
+                    >
+                      개봉 예정
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/my-list"
+                      className={({ isActive }) => (isActive ? 'active' : 'nav-tab')}
+                    >
+                      찜한 콘텐츠
+                    </NavLink>
+                  </li>
+                </ul>
+              )}
+            </div>
+            <div className="right-wrap">
+              <form>
+                <input type="text" placeholder="검색" />
+              </form>
+            </div>
+            {isTabletOrMobile && isDrop && isShow && (
+              <ul className="nav-dropdown" ref={mobNavRef}>
+                <li>
+                  <NavLink
+                    to="/now-playing"
+                    className={({ isActive }) => (isActive ? 'active' : 'nav-tab')}
+                  >
+                    현재 상영 중
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/popular"
+                    className={({ isActive }) => (isActive ? 'active' : 'nav-tab')}
+                  >
+                    인기
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/upcoming"
+                    className={({ isActive }) => (isActive ? 'active' : 'nav-tab')}
+                  >
+                    개봉 예정
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/my-list"
+                    className={({ isActive }) => (isActive ? 'active' : 'nav-tab')}
+                  >
+                    찜한 콘텐츠
+                  </NavLink>
+                </li>
+              </ul>
+            )}
+          </div>
+        </header>
+      ) : (
+        <header className="pc-header">web</header>
+      )}
+    </Fragment>
   );
 };
 
