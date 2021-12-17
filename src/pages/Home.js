9162ef20c70094bc82f2e4103166a1ba81ec8770
ldh <hani7304@gmail.com>
2021-12-18 00:57:04 +0900

    메인 페이지 작업준비

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './Home.scss';
+import { isMobile } from 'react-device-detect';
 
-const Home = () => {
-  return <div className="home-wrap">Home</div>;
+const Home = props => {
+  const { imageConfig, api_key } = props;
+  return (
+    <Fragment>
+      {isMobile ? (
+        <div className="mob-home">
+          <div className="main-backdrop">dd</div>
+          <div className="contents-wrap"></div>
+        </div>
+      ) : (
+        <div className="pc-home">PC</div>
+      )}
+    </Fragment>
+  );
 };
 
 export default Home;
